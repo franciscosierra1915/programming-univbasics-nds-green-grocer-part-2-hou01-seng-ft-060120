08902ef258afab8875ef12e4d6e6a014ccf9414d
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-28 18:23:46 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -46,7 +46,7 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  
+  binding.pry
   couponed_cart = apply_coupons(consolidate_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   total = 0 
